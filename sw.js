038a80a17c08fa37eb4be21c3ce70018812ac541
陈明康 <45618307+ChenMingK@users.noreply.github.com>
2019-07-26 09:32:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 为 fetch 事件添加事件监听器
-document.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event) {
   // 检查传入的 HTTP 请求 URL 是否请求以 .jpg 结尾的文件
   if (/\.jpg$/.test(event.request.url)) {
     // 尝试获取某个图片并用它作为替代图片来响应请求
