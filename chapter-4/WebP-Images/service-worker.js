3da4048b41d3453181ef08f274d53477c173ceeb
陈明康 <45618307+ChenMingK@users.noreply.github.com>
2019-07-26 15:36:44 +0800

    Update service-worker.js

--- chapter-4/WebP-Images/service-worker.js
+++ chapter-4/WebP-Images/service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('fetch', function(event) {
 
   // Check if the image is a jpeg
   if (/\.jpg$|.png$/.test(event.request.url)) {
-
+    console.log('拦截请求')
     // Inspect the accept header for WebP support
     var supportsWebp = false;
     if (event.request.headers.has('accept')) {
@@ -19,10 +19,11 @@ self.addEventListener('fetch', function(event) {
     if (supportsWebp) {
       // Clone the request
       var req = event.request.clone();
-
+      console.log('支持 webP')
       // Build the return URL
+      
       var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
-
+      console.log(returnUrl)
       event.respondWith(
         fetch(returnUrl, {
           mode: 'no-cors'
